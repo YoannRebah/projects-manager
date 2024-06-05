4aa3189450aa427d3a77a292086c42257f8ad9f6
yoann <yrebah@webnet.fr>
2024-06-05 17:26:41 +0200

    feat nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OverlayModule } from '../app/modules/overlay/overlay.module';
+import { MenuModule } from '../app/modules/menu/menu.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    OverlayModule
+    OverlayModule,
+    MenuModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
