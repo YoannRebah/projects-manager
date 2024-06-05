290c4e3cf57337c44fd32ab3f7dec05d0215597e
yoann <yrebah@webnet.fr>
2024-06-05 15:07:04 +0200

    fix import export components into overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoaderComponent } from '../app/components/loader/loader.component';
+import { OverlayModule } from '../app/modules/overlay/overlay.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    LoaderComponent
+    OverlayModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loader',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss'
 })
