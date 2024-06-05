f72ae733f5ed72702a283a851a32e4bc7cdb157f
yoann <yrebah@webnet.fr>
2024-06-05 13:32:37 +0200

    feat loader

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loader',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss'
 })
-export class LoaderComponent {
+export class LoaderComponent implements OnInit {
+  isLoading: boolean = false;
+  timeoutDelay: number = 5000;
+
+  ngOnInit(): void {
+    this.Show();
+    let timeout = setTimeout(()=>{
+      this.Hide();
+      clearTimeout(timeout);
+    }, this.timeoutDelay);
+  }
+
+  Show() {
+    this.isLoading = true;
+  }
+
+  Hide() {
+    this.isLoading = false;
+  }
 
 }
