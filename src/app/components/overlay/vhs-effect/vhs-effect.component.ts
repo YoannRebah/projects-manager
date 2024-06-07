8ec8df6d1e38e312e0cf05e5c30f82bb08947a1a
yoann <yrebah@webnet.fr>
2024-06-07 10:54:43 +0200

    remove timer ill be back

--- src/app/components/overlay/vhs-effect/vhs-effect.component.ts
+++ src/app/components/overlay/vhs-effect/vhs-effect.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TimerComponent } from '../../timer/timer.component';
 
 @Component({
   selector: 'app-vhs-effect',
   standalone: true,
-  imports: [CommonModule, TimerComponent],
+  imports: [CommonModule],
   templateUrl: './vhs-effect.component.html',
   styleUrl: './vhs-effect.component.scss'
 })
