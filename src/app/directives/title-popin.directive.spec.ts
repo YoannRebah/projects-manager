a964d42bcdd00c9fc595800ab021943e522ba714
yoann <yrebah@webnet.fr>
2024-06-09 01:42:50 +0200

    feat title popin directive

---
+++ src/app/directives/title-popin.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TitlePopinDirective } from './title-popin.directive';
+
+describe('TitlePopinDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TitlePopinDirective();
+    expect(directive).toBeTruthy();
+  });
+});
