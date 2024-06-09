570909e95d097f73c8950298c4ecc8b24254dba2
yoann <yrebah@webnet.fr>
2024-06-09 18:52:03 +0200

    feat time counter

--- src/app/components/shared/time-counter/time-counter.component.ts
+++ src/app/components/shared/time-counter/time-counter.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy, ChangeDetectorRef, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { interval, Subscription } from 'rxjs';
+import { TimeCounterService } from '../../../shared/services/time-counter.service';
 
 @Component({
   selector: 'app-time-counter',
@@ -14,31 +15,54 @@ export class TimeCounterComponent implements OnInit, OnDestroy {
   time: number = 0;
   timeString: string = '00:00:00';
   private subscription!: Subscription;
+  private timerSubscription!: Subscription;
   timeoutDelay: number = 5000;
 
-  constructor(private cdr: ChangeDetectorRef, private ngZone: NgZone) {}
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private ngZone: NgZone,
+    private timeCounterService: TimeCounterService
+  ) {}
 
   ngOnInit(): void {
-    let timeout = setTimeout(()=>{
-      this.initTimer();
-      clearTimeout(timeout)
-    }, this.timeoutDelay)
+    this.subscription = this.timeCounterService.isRunning$.subscribe(
+      (isRunning: boolean) => {
+        if (isRunning) {
+          this.startTimer();
+        } else {
+          this.pauseTimer();
+        }
+      }
+    );
+    let timeout = setTimeout(() => {
+      this.timeCounterService.start();
+      clearTimeout(timeout);
+    }, this.timeoutDelay);
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
   }
 
-  initTimer(): void {
+  startTimer(): void {
     this.ngZone.runOutsideAngular(() => {
       const timer$ = interval(1000);
-      this.subscription = timer$.subscribe(() => {
+      this.timerSubscription = timer$.subscribe(() => {
         this.ngZone.run(() => {
           this.updateTime();
           this.cdr.detectChanges();
         });
       });
     });
+  }
+
+  pauseTimer(): void {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
   }
 
   formatTime(number: number): string {
---
+++ src/app/shared/services/time-counter.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TimeCounterService {
+  private runningSubject = new BehaviorSubject<boolean>(false);
+  isRunning$ = this.runningSubject.asObservable();
+
+  start() {
+    this.runningSubject.next(true);
+  }
+
+  pause() {
+    this.runningSubject.next(false);
+  }
+
+  reset() {
+    this.runningSubject.next(false);
+    this.runningSubject.next(true);
+  }
+}
