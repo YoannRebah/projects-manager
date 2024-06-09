38df670511f1505fd441631654831532d60db9f8
yoann <yrebah@webnet.fr>
2024-06-09 21:45:47 +0200

    wip game

--- src/app/components/game/game/game.component.ts
+++ src/app/components/game/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,22 +9,107 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./game.component.scss']
 })
 
-export class GameComponent {
+export class GameComponent implements OnInit {
+  keyHighScore: string = 'high-score';
+  score: number = 0;
+  scoreMin: number = 0;
+  scoreMax: number = 99999;
+  highScore: number = 0;
+  scoreIsPaused: boolean = false;
   health: number = 100;
   healthMin: number = 0;
   healthMax: number = 100;
-  currentScore: number = 0;
-  highScore: number = 0;
+  gameIsOver: boolean = false;
+  mouseIsMoving: boolean = false;
+  collisionBoxIsHitted: boolean = false;
 
-  updateHealth(delta: number): void {
-    this.health += delta;
-    if(this.health <= this.healthMin) this.health = this.healthMin;
-    if(this.health >= this.healthMax) this.health = this.healthMax;
+  ngOnInit(): void {
+    this.initGame();
+  }
+
+  get playerHighScore(): number {
+    if (this.isLocalStorageAvailable()) {
+      return localStorage.getItem(this.keyHighScore) ? parseInt(localStorage.getItem(this.keyHighScore)!, 10) : 0;
+    }
+    return 0;
+  }
+
+  isLocalStorageAvailable(): boolean {
+    try {
+      const test = 'test';
+      localStorage.setItem(test, test);
+      localStorage.removeItem(test);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  initGame(): void {
+    this.highScore = this.playerHighScore;
+    this.storeDefaultHighScore();
+  }
+
+  stopGame(): void {
+    this.updateHighScore();
+  }
+
+  startScoreCounter(): void {
+    if(this.score < this.scoreMax) {
+      this.scoreIsPaused = false;
+    }
+  }
+
+  pauseScoreCounter(): void {
+    this.scoreIsPaused = true;
+  }
+
+  resumeScoreCounter(): void {
+    this.scoreIsPaused = false;
+  }
+
+  resetScoreCounter(): void {
+    this.score = this.scoreMin;
+    this.scoreIsPaused = false;
+  }
+
+  storeDefaultHighScore(): void {
+    if (this.isLocalStorageAvailable()) {
+      if(localStorage.getItem(this.keyHighScore) == null) {
+        localStorage.setItem(this.keyHighScore, JSON.stringify(0));
+      }
+    }
   }
 
   updateHighScore(): void {
-    if(this.currentScore > this.highScore) this.highScore = this.currentScore;
-    localStorage.setItem('high-score', JSON.stringify(this.highScore));
+    const highScore = this.playerHighScore;
+    if(this.score > highScore) this.highScore = this.score;
+    this.storeHighScore();
+  }
+
+  storeHighScore(): void {
+    if (this.isLocalStorageAvailable()) {
+      localStorage.setItem(this.keyHighScore, JSON.stringify(this.highScore));
+    }
+  }
+
+  updateHealth(delta: number): void {
+    this.health += delta;
+    if(this.health <= this.healthMin) {
+      this.health = this.healthMin;
+      this.gameIsOver = true;
+    }
+    if(this.health >= this.healthMax) {
+      this.health = this.healthMax;
+    }
+  }
+
+  collisionBoxHitted(): void {
+    this.collisionBoxIsHitted = true;
+    let timeout = setTimeout(()=>{
+      this.collisionBoxIsHitted = false;
+      clearTimeout(timeout);
+    }, 500)
   }
 
 }
