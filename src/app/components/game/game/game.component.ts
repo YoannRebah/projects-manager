8d12f1647a567747aac9602a9ee7e7d05a00515f
yoann <yrebah@webnet.fr>
2024-06-11 09:55:28 +0200

    feat utilities service

--- src/app/components/game/game/game.component.ts
+++ src/app/components/game/game/game.component.ts
@@ -372,25 +372,4 @@ export class GameComponent implements OnInit, OnDestroy {
     }
   }
 
-
-
-
-
-  // utilities
-  
-  // isColliding(rect1: DOMRect, rect2: DOMRect): boolean {
-  //   return !(rect1.right < rect2.left || rect1.left > rect2.right || rect1.bottom < rect2.top || rect1.top > rect2.bottom);
-  // }
-
-  // isLocalStorageAvailable(): boolean {
-  //   try {
-  //     const test = 'test';
-  //     localStorage.setItem(test, test);
-  //     localStorage.removeItem(test);
-  //     return true;
-  //   } catch (e) {
-  //     return false;
-  //   }
-  // }
-
 }
