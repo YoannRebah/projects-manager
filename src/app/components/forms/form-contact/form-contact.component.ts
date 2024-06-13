d8ead365e5567b9d83b86d6f3e3fb100bd3bbc1f
yoann <yrebah@webnet.fr>
2024-06-13 21:49:56 +0200

    feat form contact

--- src/app/components/forms/form-contact/form-contact.component.ts
+++ src/app/components/forms/form-contact/form-contact.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LocationsFrService } from '../../../shared/services/locations-fr.service';
 import { Region } from '../../../shared/models/locations-fr';
 import { UtilitiesService } from '../../../shared/services/utilities.service';
+import { templateMail } from '../../../shared/models/template-mail';
 
 @Component({
   selector: 'app-form-contact',
@@ -13,18 +15,17 @@ import { UtilitiesService } from '../../../shared/services/utilities.service';
   styleUrl: './form-contact.component.scss'
 })
 
-export class FormContactComponent implements OnInit, OnDestroy {
+export class FormContactComponent implements OnInit {
   locationsFr: Region = LocationsFrService.locationsFr;
   contactForm!: FormGroup;
+  serviceId: string = 'service_5ru7fw9';
+  templateId: string = 'template_d02jila';
+  publicKey: string = 'nAi6Eim9qL5XMDKyr';
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.initContactForm();
-  }
-
-  ngOnDestroy(): void {
-      
   }
 
   initContactForm(): void {
@@ -45,8 +46,29 @@ export class FormContactComponent implements OnInit, OnDestroy {
       this.contactForm.markAllAsTouched();
       return;
     } else {
-      console.log(this.contactForm.value);
+      const contactFormData = this.contactForm.value;
+      const templateParams = {
+        firstName: contactFormData.firstName,
+        lastName: contactFormData.lastName,
+        email: contactFormData.email,
+        tel: contactFormData.tel,
+        compagnyName: contactFormData.compagnyName,
+        compagnyPost: contactFormData.compagnyPost,
+        location: contactFormData.compagnyLocation,
+        message: contactFormData.message
+      };
+      this.sendMail(templateParams);
     }
+  }
+
+  sendMail(templateParams: {}): void {
+    console.log(templateParams)
+    emailjs.send(this.serviceId, this.templateId, templateParams, { publicKey: this.publicKey})
+    .then(() => {
+        console.log('SUCCESS!');
+      },
+      (error) => console.log('FAILED...', (error as EmailJSResponseStatus).text)
+    );
   }
 
 }
---
+++ src/app/shared/models/template-mail.ts
@@ -0,0 +1,10 @@
+export interface templateMail {
+    lastName: string,
+    firstName: string,
+    email: string,
+    tel: string,
+    compagnyName: string,
+    compagnyPost: string,
+    compagnyLocation: string,
+    message: string
+}
