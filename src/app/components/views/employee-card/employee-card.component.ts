4ba7ad7b2f6164d137a17ecba5e2a4e01c55d106
yoann <yrebah@webnet.fr>
2024-06-15 13:53:51 +0200

    fix optimise employeeCard animations

--- src/app/components/views/employee-card/employee-card.component.ts
+++ src/app/components/views/employee-card/employee-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, AfterViewInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, Renderer2, ViewChild, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,50 +8,59 @@ import { CommonModule } from '@angular/common';
   templateUrl: './employee-card.component.html',
   styleUrls: ['./employee-card.component.scss']
 })
-
 export class EmployeeCardComponent implements AfterViewInit {
 
   @ViewChild('employeeCard') employeeCard!: ElementRef;
 
   height!: number;
   width!: number;
+  animationFrameId: any;
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, private ngZone: NgZone) {}
 
   ngAfterViewInit(): void {
     const nativeElement = this.employeeCard.nativeElement;
     this.height = nativeElement.clientHeight;
     this.width = nativeElement.clientWidth;
-    this.addMouseEvents();
+    this.ngZone.runOutsideAngular(() => this.addMouseEvents());
   }
 
   addMouseEvents(): void {
     const card = this.employeeCard.nativeElement;
 
-    this.renderer.listen(card, 'mousemove', (event) => {
-      requestAnimationFrame(() => {
-        const xVal = event.layerX;
-        const yVal = event.layerY;
-        const yRotation = 20 * ((xVal - this.width / 2) / this.width);
-        const xRotation = -20 * ((yVal - this.height / 2) / this.height);
-        const transformString = `perspective(500px) scale(1.1) rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
+    const onMouseMove = (event: MouseEvent) => {
+      const xVal = event.layerX;
+      const yVal = event.layerY;
+      const yRotation = 20 * ((xVal - this.width / 2) / this.width);
+      const xRotation = -20 * ((yVal - this.height / 2) / this.height);
+      const transformString = `perspective(500px) scale(1.1) rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
+
+      if (this.animationFrameId) {
+        cancelAnimationFrame(this.animationFrameId);
+      }
+      this.animationFrameId = requestAnimationFrame(() => {
         this.renderer.setStyle(card, 'transform', transformString);
       });
-    });
+    };
 
-    this.renderer.listen(card, 'mouseout', () => {
+    const onMouseOut = () => {
       const transformString = 'perspective(500px) scale(1) rotateX(0) rotateY(0)';
       this.renderer.setStyle(card, 'transform', transformString);
-    });
+    };
 
-    this.renderer.listen(card, 'mousedown', () => {
+    const onMouseDown = () => {
       const transformString = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)';
       this.renderer.setStyle(card, 'transform', transformString);
-    });
+    };
 
-    this.renderer.listen(card, 'mouseup', () => {
+    const onMouseUp = () => {
       const transformString = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)';
       this.renderer.setStyle(card, 'transform', transformString);
-    });
+    };
+
+    this.renderer.listen(card, 'mousemove', onMouseMove);
+    this.renderer.listen(card, 'mouseout', onMouseOut);
+    this.renderer.listen(card, 'mousedown', onMouseDown);
+    this.renderer.listen(card, 'mouseup', onMouseUp);
   }
 }
