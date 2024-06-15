5330585ff77b2369af402bbc3206a752be3bcda8
yoann <yrebah@webnet.fr>
2024-06-15 23:10:15 +0200

    wip popin

--- src/app/components/shared/popin/popin.component.ts
+++ src/app/components/shared/popin/popin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { PopinService } from '../../../shared/services/components/popin.service';
@@ -10,18 +10,22 @@ import { PopinService } from '../../../shared/services/components/popin.service'
   templateUrl: './popin.component.html',
   styleUrl: './popin.component.scss'
 })
-export class PopinComponent implements OnInit, OnDestroy {
+
+export class PopinComponent implements AfterViewInit, OnDestroy {
   private isVisibleSubscription!: Subscription;
   isVisible: boolean = false;
 
-  @Input() id?: string;
+  @Input() id?: string = 'popin-default-id';
+  @Input() position?: string = 'absolute';
   @Input() img?: string; // error | valid | mail | help
   @Input() titleText?: string;
   @Input() contentText?: string;
 
-  constructor(private popinService: PopinService) {}
+  constructor(
+    private popinService: PopinService
+  ) {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     if (this.id) {
       this.subscribeIsVisible();
     }
@@ -33,7 +37,9 @@ export class PopinComponent implements OnInit, OnDestroy {
 
   subscribeIsVisible(): void {
     this.isVisibleSubscription = this.popinService.isVisible$(this.id!).subscribe({
-      next: (isVisible) => this.isVisible = isVisible,
+      next: (isVisible) => {
+        this.isVisible = isVisible;
+      },
       error: (e) => console.error('error subscribeIsVisible', e)
     });
   }
@@ -53,4 +59,5 @@ export class PopinComponent implements OnInit, OnDestroy {
   onClickHide(): void {
     this.hide();
   }
+
 }
