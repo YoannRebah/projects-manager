1aa773870a505fbe9980c1784dc891ed26f4a95a
yoann <yrebah@webnet.fr>
2024-06-15 20:08:24 +0200

    wip popin

---
+++ src/app/components/shared/popin/popin.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PopinService } from '../../../shared/services/components/popin.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-popin',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './popin.component.html',
+  styleUrl: './popin.component.scss'
+})
+export class PopinComponent implements OnInit, OnDestroy {
+  private isVisibleSubscription!: Subscription;
+  isVisible: boolean = false;
+
+  @Input() id?: string;
+
+  constructor(private popinService: PopinService) {}
+
+  ngOnInit(): void {
+    this.subscribeIsVisible();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeIsVisible();
+  }
+
+  subscribeIsVisible(): void {
+    this.isVisibleSubscription = this.popinService.isVisible$.subscribe({
+      next: (isVisible) => {
+        this.isVisible = isVisible;
+      },
+      error: (e) => console.error('error subscribeIsVisible', e)
+    })
+  }
+
+  unsubscribeIsVisible(): void {
+    if(this.isVisibleSubscription) {
+      this.isVisibleSubscription.unsubscribe();
+    }
+  }
+
+  show(): void {
+    this.popinService.show();
+  }
+
+  hide(): void {
+    this.popinService.hide();
+  }
+
+  onClickHide(): void {
+    this.hide();
+  }
+}
---
+++ src/app/shared/services/components/popin.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PopinService {
+  private isVisibleSubject = new BehaviorSubject<boolean>(true);
+  isVisible$ = this.isVisibleSubject.asObservable();
+
+  show(): void {
+    this.isVisibleSubject.next(true);
+  }
+
+  hide(): void {
+    this.isVisibleSubject.next(false);
+  }
+}
