d9653dc33b2097abe1909e79c7e10be02093677b
yoann <yrebah@webnet.fr>
2024-06-16 12:57:05 +0200

    wip terminal

--- src/app/components/terminal/terminal.component.ts
+++ src/app/components/terminal/terminal.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TerminalService } from '../../shared/services/components/terminal.service';
 import { Subscription } from 'rxjs';
+import { DatetimeService } from '../../shared/services/utilities/datetime.service';
+import { TimeoutService } from '../../shared/services/utilities/timeout.service';
 
 @Component({
   selector: 'app-terminal',
@@ -15,7 +17,12 @@ export class TerminalComponent implements OnInit, OnDestroy {
   private subscriptionIsVisible!: Subscription;
   isVisible: boolean = false;
 
-  constructor(private terminalService: TerminalService) {}
+  @ViewChild('terminalList', { static: false }) terminalList!: ElementRef;
+
+  constructor(
+    private terminalService: TerminalService,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     this.subscribeIsVisible();
@@ -45,10 +52,41 @@ export class TerminalComponent implements OnInit, OnDestroy {
   }
 
   @HostListener('window:keydown', ['$event'])
-  handleKeyDownToggleTerminal(event: KeyboardEvent) {
+  handleKeyDownTerminal(event: KeyboardEvent) {
     if (event.ctrlKey && event.key === 'q') {
       event.preventDefault();
       this.toggleTerminalIsVisible();
+      TimeoutService.setTimeout(()=>{
+        this.createRowTerminal();
+      }, 150);
+    }
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      this.createRowTerminal();
+    }
+  }
+
+  createRowTerminal(inputValue?: string): void {
+    if(this.isVisible) {
+      const rowTerminalTimestamp = DatetimeService.timestampNow;
+      const terminalList = this.terminalList.nativeElement;
+  
+      const rowTerminal = this.renderer.createElement('li');
+      const p = this.renderer.createElement('p');
+      const span = this.renderer.createElement('span');
+      const input = this.renderer.createElement('input');
+  
+      this.renderer.appendChild(p, this.renderer.createText("yoann@portfolio:"));
+      this.renderer.appendChild(span, this.renderer.createText("~#"));
+  
+      this.renderer.appendChild(p, span);
+      this.renderer.appendChild(rowTerminal, p);
+      this.renderer.appendChild(rowTerminal, input);
+  
+      if (terminalList) {
+        this.renderer.appendChild(terminalList, rowTerminal);
+        input.focus();
+      }
     }
   }
 
