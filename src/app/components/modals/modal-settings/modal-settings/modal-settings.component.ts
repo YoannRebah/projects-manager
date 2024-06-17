ae736b5411f896ceb16ebfcd7b9483d31e717fee
yoann <yrebah@webnet.fr>
2024-06-17 16:10:30 +0200

    wip modals

---
+++ src/app/components/modals/modal-settings/modal-settings/modal-settings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-settings',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-settings.component.html',
+  styleUrl: './modal-settings.component.scss'
+})
+export class ModalSettingsComponent {
+
+}
