754db32d94c62ea9d7a61e07154d391e9dac726d
yoann <yrebah@webnet.fr>
2024-06-18 12:28:56 +0200

    fix

--- src/app/components/views/list-footer/list-footer.component.ts
+++ src/app/components/views/list-footer/list-footer.component.ts
@@ -11,67 +11,73 @@ import { Anchor } from '../../../shared/models/common/anchor';
 })
 
 export class ListFooterComponent {
-  footerLinks: Anchor[] = [
-    {
-      href: '#',
-      text: "Accueil",
-      ariaLabel: 'Accueil',
-    },
-    {
-      href: '#intro',
-      text: "Introduction",
-      ariaLabel: 'Introduction',
-    },
-    {
-      href: '#skills',
-      text: "Compétences",
-      ariaLabel: 'Compétences',
-    },
-    {
-      href: '#notable-achievements',
-      text: "Réalisations Notables",
-      ariaLabel: 'Réalisations Notables',
-    },
-    {
-      href: '#tools-overview',
-      text: "Logiciels",
-      ariaLabel: 'Logiciels',
-    },
-    {
-      href: '#current-job',
-      text: "Poste Actuel",
-      ariaLabel: 'Poste Actuel',
-    },
-    {
-      href: '#location',
-      text: 'Localisation',
-      ariaLabel: 'Localisation',
-    },
-    {
-      href: '#interests',
-      text: "Centres d'intérêt",
-      ariaLabel: 'Centres d\'intérêt',
-    },
-    {
-      href: '#arcade-room',
-      text: "Salle d'Arcade",
-      ariaLabel: 'Salle d\'Arcade',
-    },
-    {
-      href: '#contact',
-      text: "Contact",
-      ariaLabel: 'Contact',
-    },
-    {
-      href: '#blog',
-      text: 'Articles du blog',
-      ariaLabel: 'Articles du blog',
-    },
-    {
-      href: 'https://github.com/YoannRebah/portfolio-angular',
-      text: 'Code Source (Git)',
-      ariaLabel: 'Code Source (Git)',
-      target: "_blank"
-    },
+  footerLinks: Anchor[][] = [
+    [
+      {
+        href: '#',
+        text: "Accueil",
+        ariaLabel: 'Accueil',
+      },
+      {
+        href: '#intro',
+        text: "Introduction",
+        ariaLabel: 'Introduction',
+      },
+      {
+        href: '#skills',
+        text: "Compétences",
+        ariaLabel: 'Compétences',
+      },
+      {
+        href: '#notable-achievements',
+        text: "Réalisations Notables",
+        ariaLabel: 'Réalisations Notables',
+      }
+    ],
+    [
+      {
+        href: '#tools-overview',
+        text: "Logiciels",
+        ariaLabel: 'Logiciels',
+      },
+      {
+        href: '#current-job',
+        text: "Poste Actuel",
+        ariaLabel: 'Poste Actuel',
+      },
+      {
+        href: '#location',
+        text: 'Localisation',
+        ariaLabel: 'Localisation',
+      },
+      {
+        href: '#interests',
+        text: "Centres d'intérêt",
+        ariaLabel: 'Centres d\'intérêt',
+      }
+    ],
+    [
+      {
+        href: '#arcade-room',
+        text: "Salle d'Arcade",
+        ariaLabel: 'Salle d\'Arcade',
+      },
+      {
+        href: '#contact',
+        text: "Contact",
+        ariaLabel: 'Contact',
+      },
+      {
+        href: '#blog',
+        text: 'Articles du blog',
+        ariaLabel: 'Articles du blog',
+      },
+      {
+        href: 'https://github.com/YoannRebah/portfolio-angular',
+        text: 'Code Source (Git)',
+        ariaLabel: 'Code Source (Git)',
+        target: "_blank"
+      },
+    ]
   ];
 }
