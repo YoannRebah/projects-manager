ab9eb8a051187e67437f02f948fe77a46202e238
yoann <yrebah@webnet.fr>
2024-06-23 11:03:58 +0200

    wip firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"portfolio-cf8c6","appId":"1:728589171041:web:2387838ea31aedcfbe6d20","storageBucket":"portfolio-cf8c6.appspot.com","apiKey":"AIzaSyBga1KOllEQ09yGv9_Gvku7mjNVGZvQfyI","authDomain":"portfolio-cf8c6.firebaseapp.com","messagingSenderId":"728589171041","measurementId":"G-M6QG9LCDVE"})), provideFirestore(() => getFirestore())]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,24 @@
 export const environment = {
     production: false,
+    admin: {
+        password: "0201",
+        cookieId: "schwarzy_movie",
+    },
     emailjs: {
-        serviceId: 'service_5ru7fw9',
-        publicKey: 'nAi6Eim9qL5XMDKyr',
+        serviceId: "service_5ru7fw9",
+        publicKey: "nAi6Eim9qL5XMDKyr",
         templateId: {
-            contact: 'template_d02jila',
-            code: 'template_lb5vklj'
+            contact: "template_d02jila",
+            code: "template_lb5vklj",
         }
     },
-    admin: {
-        password: '0201',
-        cookieId: 'schwarzy_movie'
+    firebaseConfig: {
+        apiKey: "AIzaSyBga1KOllEQ09yGv9_Gvku7mjNVGZvQfyI",
+        authDomain: "portfolio-cf8c6.firebaseapp.com",
+        projectId: "portfolio-cf8c6",
+        storageBucket: "portfolio-cf8c6.appspot.com",
+        messagingSenderId: "728589171041",
+        appId: "1:728589171041:web:2387838ea31aedcfbe6d20",
+        measurementId: "G-M6QG9LCDVE",
     }
 };
