1aacaa11dd2e46d306f0cc776a48729190769ab0
yoann <yrebah@webnet.fr>
2024-06-25 00:41:09 +0200

    fix ScrollDownFadeDirective

--- src/app/shared/directives/scroll-down-fade.directive.ts
+++ src/app/shared/directives/scroll-down-fade.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, inject } from '@angular/core';
 import { WindowRefService } from '../services/utilities/window-ref.service';
 
 @Directive({
@@ -9,28 +9,33 @@ import { WindowRefService } from '../services/utilities/window-ref.service';
 export class ScrollDownFadeDirective {
   private lastScrollTop: number = 0;
   private scrollDownFaseClassName: string = 'opacity-50';
+  mouseIsInsideElement: boolean = false;
+  elementRef = inject(ElementRef);
+  renderer = inject(Renderer2);
+  windowRefService = inject(WindowRefService);
 
-  constructor(
-    private el: ElementRef, 
-    private renderer: Renderer2,
-    private windowRefService: WindowRefService
-  ) {}
+  constructor() {}
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     let scrollTop = this.windowRefService.windowRef.scrollY
-
-    if (scrollTop > this.lastScrollTop) {
-      this.renderer.addClass(this.el.nativeElement, this.scrollDownFaseClassName);
+    if (scrollTop > this.lastScrollTop && !this.mouseIsInsideElement) {
+      this.renderer.addClass(this.elementRef.nativeElement, this.scrollDownFaseClassName);
     } else {
-      this.renderer.removeClass(this.el.nativeElement, this.scrollDownFaseClassName);
+      this.renderer.removeClass(this.elementRef.nativeElement, this.scrollDownFaseClassName);
     }
     this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
   }
 
   @HostListener('mouseenter')
   onMouseEnter() {
-    this.renderer.removeClass(this.el.nativeElement, this.scrollDownFaseClassName);
+    this.mouseIsInsideElement = true;
+    this.renderer.removeClass(this.elementRef.nativeElement, this.scrollDownFaseClassName);
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.mouseIsInsideElement = false;
   }
 
 }
