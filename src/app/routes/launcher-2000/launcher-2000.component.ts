e6d84d461ca10af36161c07d352018061d890219
yoann <yrebah@webnet.fr>
2024-06-25 00:14:43 +0200

    wip game

--- src/app/routes/launcher-2000/launcher-2000.component.ts
+++ src/app/routes/launcher-2000/launcher-2000.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { GameComponent } from '../../components/content/game/game.component';
 import { GameService } from '../../shared/services/components/game.service';
+import { VhsEffectComponent } from '../../components/base/vhs-effect/vhs-effect.component';
+import { VhsEffectService } from '../../shared/services/components/vhs-effect.service';
 
 @Component({
   selector: 'app-launcher-2000',
   standalone: true,
-  imports: [GameComponent],
+  imports: [GameComponent, VhsEffectComponent],
   templateUrl: './launcher-2000.component.html',
   styleUrl: './launcher-2000.component.scss'
 })
 
 export class Launcher2000Component implements OnInit, OnDestroy {
   gameService = inject(GameService);
+  vhsEffectService = inject(VhsEffectService);
 
   constructor() {}
 
@@ -20,7 +23,7 @@ export class Launcher2000Component implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-      this.gameService.hide();
+    this.gameService.hide();
   }
 
 }
