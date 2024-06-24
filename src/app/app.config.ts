e042ddc6318376a705a4e36fef0708eba64b901c
yoann <yrebah@webnet.fr>
2024-06-24 14:41:36 +0200

    wip login register

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getAuth, provideAuth } from '@angular/fire/auth';
+import { routes } from './app.routes';
 import { environment } from '../environments/environment';
 
+import { provideHttpClient } from '@angular/common/http';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), 
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideHttpClient(),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), 
-    provideFirestore(() => getFirestore()),
-    provideAuth(()=> getAuth())
-  ]
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ],
 };
