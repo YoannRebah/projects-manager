3a396ad1c57a3f9f1ecc8134b1b474095b30ab67
yoann <yrebah@webnet.fr>
2024-06-24 15:23:32 +0200

    wip login register

--- src/app/routes/register/register.component.ts
+++ src/app/routes/register/register.component.ts
@@ -3,15 +3,12 @@ import { H1Component } from '../../components/base/h1/h1.component';
 import { HeaderComponent } from '../../components/layout/header/header.component';
 import { FormRegisterComponent } from '../../components/content/form-register/form-register.component';
 
-import { VhsEffectComponent } from '../../components/base/vhs-effect/vhs-effect.component';
-
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
     H1Component, 
     HeaderComponent,
-    VhsEffectComponent,
     FormRegisterComponent,
   ],
   templateUrl: './register.component.html',
