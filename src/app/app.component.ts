6ee9ce3ae94d0d63dd8a78a2fe25673f2d193694
yoann <yrebah@webnet.fr>
2024-06-29 16:21:55 +0200

    wip 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoaderHourglassComponent } from './components/base/loader-hourglass/loader-hourglass.component';
-import { ModalUserAccountComponent } from './components/content/modal-user-account/modal-user-account.component';
-import { ModalSettingsComponent } from './components/content/modal-settings/modal-settings.component';
-import { ModalTerminalHelpComponent } from './components/content/modal-terminal-help/modal-terminal-help.component';
+import { ModalUserAccountComponent } from './components/modals/modal-user-account/modal-user-account.component';
+import { ModalSettingsComponent } from './components/modals/modal-settings/modal-settings.component';
+import { ModalTerminalHelpComponent } from './components/modals/modal-terminal-help/modal-terminal-help.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomepageComponent } from './routes/homepage/homepage.component';
 import { LoginComponent } from './routes/login/login.component';
 import { RegisterComponent } from './routes/register/register.component';
 import { Launcher2000Component } from './routes/launcher-2000/launcher-2000.component';
+import { PageNotFoundComponent } from './routes/page-not-found//page-not-found.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'launcher-2000', component: Launcher2000Component },
+    { path: '**', component: PageNotFoundComponent },
 ];
