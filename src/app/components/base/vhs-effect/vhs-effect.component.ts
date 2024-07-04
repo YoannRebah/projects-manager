8463aee770ffea3b5dd3899e386182471e73e19f
yoann <yrebah@webnet.fr>
2024-07-04 14:02:17 +0200

    remove time-counter

--- src/app/components/base/vhs-effect/vhs-effect.component.ts
+++ src/app/components/base/vhs-effect/vhs-effect.component.ts
@@ -1,35 +1,28 @@
 import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { TimeCounterComponent } from '../time-counter/time-counter.component';
 import { VhsEffectService } from '../../../shared/services/components/vhs-effect.service';
 
 @Component({
   selector: 'app-vhs-effect',
   standalone: true,
-  imports: [TimeCounterComponent],
+  imports: [],
   templateUrl: './vhs-effect.component.html',
   styleUrl: './vhs-effect.component.scss'
 })
 
 export class VhsEffectComponent implements OnInit, OnDestroy {
   private isVisibleSubscription!: Subscription;
-  // private footerIsVisibleSubscription!: Subscription;
   isVisible: boolean = true;
-  // footerIsFlashing: boolean = false;
-  // footerIsVisible: boolean = false;
   vhsEffectService = inject(VhsEffectService);
 
   constructor() {}
 
   ngOnInit(): void {
     this.subscribeIsVisible()
-    // this.subscribeFooterIsVisible();
-    // this.showFooter();
   }
 
   ngOnDestroy(): void {
     this.unsubscribeIsVisible();
-    // this.unsubscribeFooterIsVisible();
   }
 
   // is visible
@@ -47,29 +40,5 @@ export class VhsEffectComponent implements OnInit, OnDestroy {
       this.isVisibleSubscription.unsubscribe();
     }
   }
-
-  // footer is visible
-  // subscribeFooterIsVisible(): void {
-  //   this.footerIsVisibleSubscription = this.vhsEffectService.footerIsVisible$.subscribe({
-  //     next: (footerIsVisible) => {
-  //       this.footerIsVisible = footerIsVisible;
-  //     },
-  //     error: (e) => console.error('error subscribeFooterIsVisible', e)
-  //   })
-  // }
-
-  // unsubscribeFooterIsVisible(): void {
-  //   if(this.footerIsVisibleSubscription) {
-  //     this.footerIsVisibleSubscription.unsubscribe();
-  //   }
-  // }
-
-  // showFooter(): void {
-  //   this.vhsEffectService.showFooter();
-  // }
-
-  // hideFooter(): void {
-  //   this.vhsEffectService.hideFooter();
-  // }
 
 }
